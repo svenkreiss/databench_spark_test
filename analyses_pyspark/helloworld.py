0bc16e3eb108a2a831a16d3241a33cf42be60f49
Sven Kreiss <sk@svenkreiss.com>
2014-07-24 16:38:17 -0400

    First working version with databench/dev-0.3.

--- analyses_pyspark/helloworld.py
+++ analyses_pyspark/helloworld.py
@@ -1,19 +1,34 @@
 """Helloworld example for pyspark in databench."""
 
+import databench_py
 
-import databench
-
+import logging
 from pyspark import SparkContext
 
+# logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger('helloworld_kernel')
 
-ANALYSIS = databench.Analysis('helloworld', __name__)
-ANALYSIS.description = __doc__
+
+class HelloWorld(databench_py.Analysis):
+    def __init__(self, name, description):
+        databench_py.Analysis.__init__(self, name, description)
+
+        self.sc = SparkContext("local", "App Name", pyFiles=[
+            'analyses_pyspark/helloworld.py'
+        ])
+
+    def on_connect(self):
+        self.emit('log', 'on_connect: Hello World')
+
+    def on_frontendmessage(self, msg):
+        logger.debug('on_frontendmessage: '+str(msg))
+        self.emit('log', 'on_frontendmessage: Hello World')
+
+        do_something = self.sc.parallelize(range(5), 5).glom().collect()
+        print do_something
+        self.emit('log', str(do_something))
 
 
-@ANALYSIS.signals.on('connect')
-def onconnect():
-    """Run as soon as a browser connects to this."""
-    ANALYSIS.signals.emit('status', {'message': 'Hello World'})
-
-    sc = SparkContext("local", "App Name", pyFiles=['helloworld.py'])
-    ANALYSIS.signals.emit('spark_context', sc)
+if __name__ == "__main__":
+    hw = HelloWorld('helloworld', __doc__)
+    hw.event_loop()
